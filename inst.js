188707c0889279ed601ea113daf1dd12121a9e90
NegruVictor <120954772+NegruVictor@users.noreply.github.com>
2023-02-14 11:01:52 +0200

    Update inst.js

--- inst.js
+++ inst.js
@@ -5,7 +5,6 @@ const refreshPage = () => {
 refreshButton.addEventListener('click', refreshPage)
 
 //the winning button selection algorithm
-
 const buttonNumber = document.querySelector('.btn');
 let number;
 function numberButton() {
@@ -28,27 +27,3 @@ function numberButton() {
       buton.addEventListener("click", onClick);
     }
 }
-
-
-
-// let value = generateRandomInteger(3);
-// let result1 = null;
-// let result2 = null;
-// let result3 = null;
-// if (value === 3) {
-//   result1 = "congratulations";
-// } else if (value === 2) {
-//   result2 = "congratulations";
-// } else {
-//   result3 = "congratulations";
-// }
-// function premium_1() {
-//   document.getElementById("display").innerHTML = result1;
-// }
-// function premium_2() {
-//   document.getElementById("display").innerHTML = result2;
-// }
-// function premium_3(){
-//   document.getElementById("display").innerHTML = result3;
-// }
-
