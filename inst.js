9bccc4eddfb007bb549926488015a2f5538f6d8c
NegruVictor <120954772+NegruVictor@users.noreply.github.com>
2023-03-01 10:23:40 +0200

    Add files via upload

--- inst.js
+++ inst.js
@@ -3,26 +3,38 @@ const refreshPage = () => {
   location.reload();
 }
 refreshButton.addEventListener('click', refreshPage)
-
 //the winning button selection algorithm
 const buttonNumber = document.querySelector('.btn');
-let number;
 function numberButton() {
-  let winningButton;
   const onClick = function() {
-    winningButton = winningButton ? winningButton : Math.floor(Math.random() * number);
+    let winningButton = generateNumber(number)
     if (winningButton == this.id) {
-      document.getElementById("display").innerHTML = "Congratulations! You've guessed the button!";
+      document.getElementById("display").innerHTML ="Congratulations! You've guessed the button!"
     } else {
       alert("Sorry, wrong button!")
     }
   }
-    number = document.getElementById("boxNumber").value;
-    for (let i = 1; i <= number; ++i) {
-      const buton = document.createElement("button")
-      document.querySelector('div').appendChild(buton)
-      buton.id = i;
-      buton.innerText = " >> " + i + " << ";
-      buton.addEventListener("click", onClick);
-    }
+  let number = document.getElementById("boxNumber").value;
+  for (let i = 1; i <= number; ++i) {
+    const buton = document.createElement("button")
+    document.querySelector('div1').appendChild(buton)
+    buton.style.backgroundColor = '#25d67e';
+    buton.style.borderRadius = '10px';
+    buton.style.height = '30px';
+    buton.style.padding = '3px 13px';
+    buton.style.fontSize = '20px';
+    buton.style.marginRight = '8px';
+    buton.id = i;
+    buton.innerText = i;
+    buton.addEventListener("click", onClick);
+  }
 }
+function generateNumber(number){
+  let n = Math.floor(Math.random() * number); 
+  if (n == number) {
+    return --n;
+  }
+  return n;
+}
+
+
